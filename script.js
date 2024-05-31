79246ec971ddd03bdf77c9b5d758e9bd8e929dd5
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2024-05-31 16:28:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -96,6 +96,76 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //contagem
+document.querySelectorAll('.menu-lateral a').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelector('.menu-lateral').classList.remove('open');
+        document.querySelector('.imenu').style.opacity = '1';
+    });
+});
+
+document.querySelector('.imenu').addEventListener('click', function() {
+    document.querySelector('.menu-lateral').classList.add('open');
+    document.querySelector('.imenu').style.opacity = '1';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.menu-lateral').classList.remove('open');
+    document.querySelector('.imenu').style.opacity = '1';
+});
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function toggleAnimation() {
+    var textoDeslizante = document.querySelector('.texto-deslizante');
+    if (isElementInViewport(textoDeslizante)) {
+        textoDeslizante.classList.add('aparecer');
+        window.removeEventListener('scroll', toggleAnimation);
+    }
+}
+
+window.addEventListener('scroll', toggleAnimation);
+toggleAnimation();
+
+const panel = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const valores = document.querySelectorAll('.valor');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    valores.forEach(valor => {
+        observer.observe(valor);
+    });
+});
+
 const counters = document.querySelectorAll('.counter');
 
 const updateCounter = (counter) => {
@@ -116,16 +186,16 @@ const updateCounter = (counter) => {
     incrementCounter();
 };
 
-const observer = new IntersectionObserver((entries, observer) => {
+const counterObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             const counter = entry.target;
             updateCounter(counter);
-            observer.unobserve(counter); // Desativar o observador após a atualização do contador
+            observer.unobserve(counter);
         }
     });
-}, { threshold: 0.1 }); // Ajuste o threshold conforme necessário
+}, { threshold: 0.1 });
 
 counters.forEach(counter => {
-    observer.observe(counter);
+    counterObserver.observe(counter);
 });
